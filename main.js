039f1f1c1053ef18e50bfe8f11707a47c6e847ef
Raymond Arceneaux <raymond.techcoder@gmail.com>
2018-08-28 19:09:27 -0500

    add main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// const cats = ['rat','bat','mice'];
+// console.log('1st cat',cats[0]);
+
+
+// const favoriteAnimal = 'cow','bear', 'dog';
+// const favoriteAnimalArray = favoriteAnimal.split('');
+// console.log('favorAnimanArray',favoriteAnimalArray);
+
+
+// const numArray =[ 1,2,3,4,5];
+// const newNum = numArray.join('');
+// console.log('newNum', newNum);
+
+const palChecker = (word) =>{ 
+let wordReverse = word.split('');
+const 
